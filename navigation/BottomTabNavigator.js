eeef1175e91761afa505277d333f8ebd3bdb843e
github username <github email address>
2019-10-02 19:48:46 -0700

    react tab navigation

---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import ScreenOne from '../screens/ScreenOne';
+import ScreenTwo from '../screens/ScreenTwo';
+
+const BottomTabNavigator = createBottomTabNavigator({
+  One: ScreenOne,
+  Two: ScreenTwo
+}, {
+  tabBarOptions: {
+    showLabel: false
+  }
+});
+
+export default BottomTabNavigator;
---
+++ screens/ScreenTwo.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
+
+const TabIcon = (props) => (
+  <Ionicons
+    name={'md-apps'}
+    size={35}
+    color={props.focused ? 'grey' : 'darkgrey'}
+  />
+)
+
+export default class ScreenTwo extends React.Component {
+
+  static navigationOptions = {
+    tabBarIcon: TabIcon
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Screen Two</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
